daed2b10a8b6f4dbad5f8ec3972284efc2f9e18d
dhayananthdevaraj <dhayan.d3@gmail.com>
2022-11-13 07:57:32 +0530

    created chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat';
+title : String = "";
+chat:any = []
+chat2:any = []
+person1chat(){
+this.chat.push({"name":"person 1","message":this.title})
+this.title = ""
+}
+person2chat(){
+this.chat2.push({"name":"person 2","message":this.title})
+this.title = ""
+}
 }
